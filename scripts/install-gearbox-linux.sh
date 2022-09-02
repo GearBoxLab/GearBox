5d5c7f7a78c73905c14214c773d15c9dc36a99b8
Leo <leo.on.the.earth@gmail.com>
2022-09-03 05:26:46 +0800

    Only download gearbox binary with newer version

--- scripts/install-gearbox-linux.sh
+++ scripts/install-gearbox-linux.sh
@@ -2,8 +2,19 @@
 
 GEARBOX_LATEST_VERSION="$(curl -s 'https://raw.githubusercontent.com/GearBoxLab/GearBox/master/version/latest')"
 GEARBOX_DOWNLOAD_URL=https://github.com/GearBoxLab/GearBox/releases/download/$GEARBOX_LATEST_VERSION/gearbox-$GEARBOX_LATEST_VERSION-linux-amd64
+DOWNLOAD_GEARBOX=1
 
-sudo curl -sLo /usr/local/bin/gearbox $GEARBOX_DOWNLOAD_URL
-sudo chmod 755 /usr/local/bin/gearbox
+if [[ -f /usr/local/bin/gearbox ]]; then
+  INSTALLED_GEARBOX_VERSION="$(/usr/local/bin/gearbox version --no-ansi)"
+
+  if [[ $INSTALLED_GEARBOX_VERSION == *"$GEARBOX_LATEST_VERSION"* ]]; then
+    DOWNLOAD_GEARBOX=0
+  fi
+fi
+
+if [[ 1 == $DOWNLOAD_GEARBOX ]]; then
+  sudo curl -sLo /usr/local/bin/gearbox $GEARBOX_DOWNLOAD_URL
+  sudo chmod 755 /usr/local/bin/gearbox
+fi
 
 /usr/local/bin/gearbox help
--- scripts/install-gearbox-windows.bat
+++ scripts/install-gearbox-windows.bat
@@ -2,23 +2,38 @@
 
 SET VERSION_FILE_PATH=%TEMP%\GearBox-latest-version.txt
 SET REFRESHENV_PATH=%TEMP%\RefreshEnv.cmd
+SET DOWNLOAD_GEARBOX=1
 
 bitsadmin.exe /transfer "Get GearBox latest version" https://raw.githubusercontent.com/GearBoxLab/GearBox/master/version/latest %VERSION_FILE_PATH%
 SET /p GEARBOX_LATEST_VERSION=<%VERSION_FILE_PATH%
-
 SET GEARBOX_DOWNLOAD_URL=https://github.com/GearBoxLab/GearBox/releases/download/%GEARBOX_LATEST_VERSION%/gearbox-%GEARBOX_LATEST_VERSION%-windows-amd64.exe
 
-MKDIR %USERPROFILE%\.gearbox\bin
+IF NOT EXIST "%USERPROFILE%\.gearbox\bin" MKDIR "%USERPROFILE%\.gearbox\bin"
 
-bitsadmin.exe /transfer "Download gearbox.exe" %GEARBOX_DOWNLOAD_URL% %USERPROFILE%\.gearbox\bin\gearbox.exe
-bitsadmin.exe /transfer "Download chocolatey's RefreshEnv.cmd" https://raw.githubusercontent.com/chocolatey/choco/f924d47fb4177a9a34ff0c2bf995938b5c12800b/src/chocolatey.resources/redirects/RefreshEnv.cmd %REFRESHENV_PATH%
+IF EXIST "%USERPROFILE%\.gearbox\bin\gearbox.exe" (
+  SET INSTALLED_GEARBOX_VERSION=???
+  FOR /F "tokens=* USEBACKQ" %%A IN (`"%USERPROFILE%\.gearbox\bin\gearbox.exe" version --no-ansi`) DO (
+    SET INSTALLED_GEARBOX_VERSION=%%A
+  )
 
-%USERPROFILE%\.gearbox\bin\gearbox.exe init
+  CALL SET TEST_INSTALLED_GEARBOX_VERSION=%%INSTALLED_GEARBOX_VERSION:%GEARBOX_LATEST_VERSION%=%%
 
-ECHO.
+  IF NOT "%TEST_INSTALLED_GEARBOX_VERSION%" EQU "%INSTALLED_GEARBOX_VERSION%" (
+    SET DOWNLOAD_GEARBOX=0
+  )
+)
 
-CALL %REFRESHENV_PATH%
-DEL /Q %REFRESHENV_PATH%
+IF DOWNLOAD_GEARBOX == 1 (
+  bitsadmin.exe /transfer "Download gearbox.exe" %GEARBOX_DOWNLOAD_URL% %USERPROFILE%\.gearbox\bin\gearbox.exe
+  bitsadmin.exe /transfer "Download chocolatey's RefreshEnv.cmd" https://raw.githubusercontent.com/chocolatey/choco/f924d47fb4177a9a34ff0c2bf995938b5c12800b/src/chocolatey.resources/redirects/RefreshEnv.cmd %REFRESHENV_PATH%
+
+  %USERPROFILE%\.gearbox\bin\gearbox.exe init
+
+  ECHO.
+
+  CALL %REFRESHENV_PATH%
+  DEL /Q %REFRESHENV_PATH%
+)
 
 ECHO.
 
